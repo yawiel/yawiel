1bea8ce181c97bcf2456542b0ea7eef0f0ae1909
robertohueso <robertohueso96@gmail.com>
2019-06-28 14:11:45 +0200

    Update EAM test

--- src/yawiel/tests/eam_test.cpp
+++ src/yawiel/tests/eam_test.cpp
@@ -20,18 +20,21 @@ using namespace yawiel::colext;
 
 BOOST_AUTO_TEST_SUITE(EAMTest);
 
+typedef NGramCounter<std::string> Counter;
 const std::string text1 = "this is a test a test";
 
 BOOST_AUTO_TEST_CASE(PMITest)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
+  PMI<Counter> pmi(counter);
+  pmi.Precompute(3);
 
   vector<size_t> gram1 {0, 1};
   vector<size_t> gram2 {2};
 
-  const double result = PMI<string>::Evaluate(gram1, gram2, counter);
+  const double result = pmi.Evaluate(gram1, gram2);
   // Compare against a manually calculated PMI value.
   BOOST_REQUIRE_CLOSE(result, 1.90833401257, 0.1);
 }
@@ -40,13 +43,15 @@ BOOST_AUTO_TEST_CASE(DiceTest)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
+  Dice<Counter> dice(counter);
+  dice.Precompute(3);
   const auto& vocab = corpus.GetVocabulary();
 
   vector<size_t> gram1 {vocab.at("this"), vocab.at("is")};
   vector<size_t> gram2 {vocab.at("a")};
 
-  const double result = Dice<string>::Evaluate(gram1, gram2, counter);
+  const double result = dice.Evaluate(gram1, gram2);
   // Compare against a manually calculated Dice value.
   BOOST_REQUIRE_CLOSE(result, 0.6666666666666, 0.1);
 }
@@ -55,41 +60,67 @@ BOOST_AUTO_TEST_CASE(G1Test)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
   vector<size_t> aTestA {2, 3, 2};
-  const double G1Result =
-    G1<string, PMI<string>>::Evaluate(aTestA, counter);
+  PMI<Counter> pmi(counter);
+  G1<PMI<Counter>> g1(counter, pmi);
+  g1.Precompute(3);
+
+  const double G1Result = g1.Evaluate(aTestA);
+
   // Manual result.
   vector<size_t> aTest {2, 3};
   vector<size_t> a {2};
   vector<size_t> testA {3, 2};
-  const double PMIResult1 = PMI<string>::Evaluate(aTest, a, counter);
-  const double PMIResult2 = PMI<string>::Evaluate(a, testA, counter);
+  const double PMIResult1 = pmi.Evaluate(aTest, a);
+  const double PMIResult2 = pmi.Evaluate(a, testA);
   const double ManualResult = (PMIResult1 + PMIResult2) / 2.0;
   BOOST_REQUIRE_CLOSE(G1Result, ManualResult, 1e-8);
+}
+
+BOOST_AUTO_TEST_CASE(G0PMITest)
+{
+  Corpus<string> corpus;
+  corpus.LoadString(text1);
+  Counter counter(corpus);
+  vector<size_t> aTestA {2, 3, 2};
+  PMI<Counter> pmi(counter);
+  G0<PMI<Counter>> g0(counter, pmi);
+  g0.Precompute(3);
+
+  const double g0Result = g0.Evaluate(aTestA);
+
+  // Manual result.
+  const double numerator = 1.0 / 4.0;
+  const double denominator = std::pow((2.0 / 6.0), 3.0);
+  const double ManualResult = std::log2(numerator / denominator);
+  BOOST_REQUIRE_CLOSE(g0Result, ManualResult, 1e-8);
 }
 
 BOOST_AUTO_TEST_CASE(G2Test)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
   const auto& vocab = corpus.GetVocabulary();
+  Dice<Counter> dice(counter);
+  G2<Dice<Counter>> g2(counter, dice);
+  g2.Precompute(4);
 
   vector<size_t> gram {vocab.at("is"),
                        vocab.at("a"),
                        vocab.at("test"),
                        vocab.at("a")};
 
-  const double g2Result = G2<string, Dice<string>>::Evaluate(gram, counter);
+  const double g2Result = g2.Evaluate(gram);
 
   // Manual result.
   vector<size_t> isA {gram[0], gram[1]};
   vector<size_t> testA {gram[2], gram[3]};
   vector<size_t> isATest {gram[0],gram[1], gram[2]};
   vector<size_t> a {gram[3]};
-  const double diceResult1 = Dice<string>::Evaluate(isA, testA, counter);
-  const double diceResult2 = Dice<string>::Evaluate(isATest, a, counter);
+  const double diceResult1 = dice.Evaluate(isA, testA);
+  const double diceResult2 = dice.Evaluate(isATest, a);
   const double manualResult = (diceResult1 + diceResult2) / 2.0;
   BOOST_REQUIRE_CLOSE(g2Result, manualResult, 1e-8);
 }
@@ -98,8 +129,11 @@ BOOST_AUTO_TEST_CASE(G3Test)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
   const auto& vocab = corpus.GetVocabulary();
+  PMI<Counter> pmi(counter);
+  G3<PMI<Counter>> g3(counter, pmi);
+  g3.Precompute(5);
 
   vector<size_t> gram {vocab.at("is"),
                        vocab.at("a"),
@@ -107,17 +141,17 @@ BOOST_AUTO_TEST_CASE(G3Test)
                        vocab.at("a"),
                        vocab.at("test")};
 
-  const double g3Result = G3<string, PMI<string>>::Evaluate(gram, counter);
+  const double g3Result = g3.Evaluate(gram);
 
   // Manual result.
   vector<size_t> is {gram[0]};
   vector<size_t> a {gram[1]};
   vector<size_t> test {gram[2]};
 
-  const double pmiResult1 = PMI<string>::Evaluate(is, a, counter);
-  const double pmiResult2 = PMI<string>::Evaluate(a, test, counter);
-  const double pmiResult3 = PMI<string>::Evaluate(test, a, counter);
-  const double pmiResult4 = PMI<string>::Evaluate(a, test, counter);
+  const double pmiResult1 = pmi.Evaluate(is, a);
+  const double pmiResult2 = pmi.Evaluate(a, test);
+  const double pmiResult3 = pmi.Evaluate(test, a);
+  const double pmiResult4 = pmi.Evaluate(a, test);
   const double manualResult =
       (pmiResult1 + pmiResult2 + pmiResult3 + pmiResult4) / (5 - 1);
 
@@ -128,8 +162,11 @@ BOOST_AUTO_TEST_CASE(G4Test)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
   const auto& vocab = corpus.GetVocabulary();
+  Dice<Counter> dice(counter);
+  G4<Dice<Counter>> g4(counter, dice);
+  g4.Precompute(8);
 
   vector<size_t> gram {vocab.at("is"),
                        vocab.at("a"),
@@ -137,15 +174,13 @@ BOOST_AUTO_TEST_CASE(G4Test)
                        vocab.at("a"),
                        vocab.at("test")};
 
-  const double g4Result = G4<string, Dice<string>>::Evaluate(gram, counter);
+  const double g4Result = g4.Evaluate(gram);
 
   // Manual result.
   vector<size_t> isATestA {gram[0], gram[1], gram[2], gram[3]};
   vector<size_t> aTestATest {gram[1], gram[2], gram[3], gram[4]};
 
-  const double manualResult = Dice<string>::Evaluate(isATestA,
-                                                     aTestATest,
-                                                     counter);
+  const double manualResult = dice.Evaluate(isATestA, aTestATest);
 
   BOOST_REQUIRE_CLOSE(g4Result, manualResult, 1e-8);
 }
@@ -154,15 +189,18 @@ BOOST_AUTO_TEST_CASE(G5Test)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
   const auto& vocab = corpus.GetVocabulary();
+  Dice<Counter> dice(counter);
+  G5<Dice<Counter>> g5(counter, dice);
+  g5.Precompute(5);
 
   vector<size_t> gram {vocab.at("is"),
                        vocab.at("a"),
                        vocab.at("test"),
                        vocab.at("a")};
 
-  const double g5Result = G5<string, Dice<string>>::Evaluate(gram, counter);
+  const double g5Result = g5.Evaluate(gram);
 
   // Manual result.
   vector<size_t> is {gram[0]};
@@ -172,9 +210,9 @@ BOOST_AUTO_TEST_CASE(G5Test)
   vector<size_t> isATest {gram[0], gram[1], gram[2]};
   vector<size_t> a {gram[3]};
 
-  double manualResult = Dice<string>::Evaluate(is, aTestA, counter) +
-                        Dice<string>::Evaluate(isA, testA, counter) +
-                        Dice<string>::Evaluate(isATest, a, counter);
+  double manualResult = dice.Evaluate(is, aTestA) +
+                        dice.Evaluate(isA, testA) +
+                        dice.Evaluate(isATest, a);
   manualResult /= 3;
 
   BOOST_REQUIRE_CLOSE(g5Result, manualResult, 1e-8);
@@ -184,15 +222,18 @@ BOOST_AUTO_TEST_CASE(G6DiceTest)
 {
   Corpus<string> corpus;
   corpus.LoadString(text1);
-  NGramCounter<string> counter(corpus);
+  Counter counter(corpus);
   const auto& vocab = corpus.GetVocabulary();
+  Dice<Counter> dice(counter);
+  G6<Dice<Counter>> g6(counter, dice);
+  g6.Precompute(5);
 
   vector<size_t> gram {vocab.at("is"),
                        vocab.at("a"),
                        vocab.at("test"),
                        vocab.at("a")};
 
-  const double g6Result = G6<string, Dice<string>>::Evaluate(gram, counter);
+  const double g6Result = g6.Evaluate(gram);
 
   // Manual result.
   vector<size_t> isA {gram[0], gram[1]};
